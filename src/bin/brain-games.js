d48834e694c5f1daa51429b53991a3e213df1452
Tim Petrov <timofey1992@gmail.com>
2019-04-08 22:03:43 +0300

    move brain-games.js console.log to index.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,6 +2,5 @@
 
 import { learnName } from '..';
 
-console.log('Welcome to the Brain Games!');
 //вызов: функция узнает имя игрока и приветствует его
 learnName();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import readlineSync from 'readline-sync';
 
 // функция узнает имя игрока и приветствует его
 export const learnName = (name) => {
+  console.log('Welcome to the Brain Games!');
   const answer = readlineSync.question('May I have your name?: ');
   if (answer === '') {
     return console.log( `Hello, Player` );
